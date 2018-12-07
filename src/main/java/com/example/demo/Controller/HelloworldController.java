7d60b0484dbec8bea3cab9bc1d1f4f6b294d0c01
WILMER <W.A.COTRINA@GMAIL.COM>
2018-12-07 10:40:47 -0500

    depuracion

--- src/main/java/com/example/demo/Controller/HelloworldController.java
+++ src/main/java/com/example/demo/Controller/HelloworldController.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.demo;
+package com.example.demo.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
